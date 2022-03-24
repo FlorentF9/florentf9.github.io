74df1d62d90a9eadbcf764208f96ad83ec03a1d2
Florent Forest <florent.forest9@gmail.com>
2022-03-24 20:21:51 +0100

    Jekyll _site build + add Dockerfile

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,8 +1,4 @@
 docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll sh
 gem install rouge jekyll-default-layout liquid-md5
 jekyll build
-
 jekyll serve --incremental
-
-
-git checkout gh-pages
