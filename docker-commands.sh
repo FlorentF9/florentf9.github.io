52b0316d85aa6861b64c759963af12573f30e095
Florent Forest <florent.forest@epfl.ch>
2024-03-25 15:35:16 +0100

    Update bibbase CSS

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,8 +1,8 @@
-sudo docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll sh
-gem install rouge jekyll-default-layout liquid-md5 webrick
-jekyll build
-jekyll serve --incremental
+# Running and building site
+sudo docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it --entrypoint sh jekyll/jekyll -c "gem install rouge jekyll-default-layout liquid-md5 webrick && jekyll serve --incremental"
+sudo docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it --entrypoint sh jekyll/jekyll -c "gem install rouge jekyll-default-layout liquid-md5 webrick && jekyll build"
 
+# Pushing changes to gh-pages branch
 cd _site
 git add .
 git cm "Jekyll _site build"
