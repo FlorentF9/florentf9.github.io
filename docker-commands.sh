d0e5da1509aeb51c24df391c8a2395c742c79c6c
Florent Forest <florent.forest@epfl.ch>
2024-03-25 15:47:34 +0100

    Jekyll _site build

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,6 +1,8 @@
+# Running and building site
 sudo docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it --entrypoint sh jekyll/jekyll -c "gem install rouge jekyll-default-layout liquid-md5 webrick && jekyll serve --incremental"
 sudo docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 -it --entrypoint sh jekyll/jekyll -c "gem install rouge jekyll-default-layout liquid-md5 webrick && jekyll build"
 
+# Pushing changes to gh-pages branch
 cd _site
 git add .
 git cm "Jekyll _site build"
